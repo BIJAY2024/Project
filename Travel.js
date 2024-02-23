21674bf4ff1c5008376fb6d2332969af0018f693
BIJAY2024 <averagejoe040@gmail.com>
2024-02-23 18:05:00 +0545

    project

---
+++ Travel.js
@@ -0,0 +1,6 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    document.getElementById("menu").classList.add("smenu");
+});
+document.getElementById("exist").addEventListener("click", function() {
+    document.getElementById("menu").classList.remove("smenu");
+});
